974f16e2c8841e141b1b7f607699dbf161591f47
JustinBBD <157216852+JustinBBD@users.noreply.github.com>
2024-07-22 20:25:32 +0200

    fix null issue if no auth header

--- src/Middleware/authMiddleware.js
+++ src/Middleware/authMiddleware.js
@@ -2,10 +2,10 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 function verifyToken(req, res, next) {
-    const token = req.header('Authorization').replace('Bearer ', '');
+    const token = req.header('Authorization');
     if (!token) return res.status(401).json({ error: 'Access denied' });
     try {
-        const decoded = jwt.verify(token, JWT_SECRET_KEY);
+        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET_KEY);
         req.userName = decoded.userName;
         next();
     } catch (error) {
