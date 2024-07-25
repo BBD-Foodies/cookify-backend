217cf7d0515238203df8b4ce6d0acce5ea62557b
tristanphips <tristan.phipson@bbd.co.za>
2024-07-25 23:12:04 +0200

    mustcache

--- src/Services/OpenApiService.js
+++ src/Services/OpenApiService.js
@@ -21,13 +21,16 @@ async function getImageUrl(recipeName) {
 
     const imageUrl = await apiCallDalle2(formattedRecipeName);
 
-    imageCache[formattedRecipeName] = imageUrl;
+    if (imageUrl.mustCache) {
+        imageCache[formattedRecipeName] = imageUrl;
+    }
 
-    return imageUrl;
+    return imageUrl.imageUrl;
 }
 
 const apiCallDalle2 = async (recipeName) => {
     let image_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzCYkTtbFS6mH-PZ5e8f5vV7dD1sEOUZeMmQ&s';
+    let mustCache = false;
     try {
         const response = await openai.images.generate({
             model: "dall-e-2",
@@ -36,10 +39,11 @@ const apiCallDalle2 = async (recipeName) => {
             size: "256x256",
         });
         image_url = response.data[0].url;
+        mustCache = true;
     } catch (error) {
         console.error('Could not get AI image.');
     }
-    return image_url;
+    return { image_url, mustCache };
 }
 
 const getRecipeGenIcon = async (recipeName) => {
